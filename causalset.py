d3cd76cdb3d9b31512fbd7dafad1c1f8525c9fb4
hinchilee <66686518+hinchilee@users.noreply.github.com>
2022-10-18 14:21:55 +0100

    Add files via upload

---
+++ causalset.py
@@ -0,0 +1,8 @@
+
+class CausalSet(object): 
+    
+    def __init__(self, **kwargs): 
+        self.Set: set(CausalEvent) = 
+        self.CausalMatrix: np.array 
+        self.LinkMatrix: np.array = 
+        
