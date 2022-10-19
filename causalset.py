03f38214ef36680594e3e08c179ab8ef5571a4c0
hinchilee <66686518+hinchilee@users.noreply.github.com>
2022-10-19 22:24:17 +0100

    Add files via upload
    added visualisation as a class method

--- causalset.py
+++ causalset.py
@@ -1,5 +1,7 @@
 
 import numpy as np 
+import time 
+import matplotlib.pyplot as plt
 from scipy.optimize import fsolve
 from scipy.special import gamma 
 from itertools import combinations 
@@ -7,33 +9,47 @@ from causalsetfunctions import f, findC2, spacetime_interval
 from causalEvent import CausalEvent
 from Sprinkling import Sprinkling_Minkowski 
 
+np.random.seed(10)
+
 class CausalSet(object): 
     
     def __init__(self, **kwargs): 
         
-        self.List: list (CausalEvent) = list() 
+        self.ElementList: list (CausalEvent) = list() 
         #Sprinkling and sorting by time coordinate 
         sprinkledcoords = Sprinkling_Minkowski(dimension = kwargs.get('dimension', 2), number_of_points = kwargs.get('number_of_points', 5), bounds = kwargs.get('bounds', np.array([[0,1] for i in range(kwargs.get('dimension', 2))])))
         sprinkledcoords = sprinkledcoords[sprinkledcoords[:, 0].argsort()]
         for i, coords in enumerate(sprinkledcoords): 
-            self.List.append(CausalEvent(label = i, coordinates = coords))
+            self.ElementList.append(CausalEvent(label = i, coordinates = coords))
     
         #Create causal matrix using spacetime intervals
-        self.CausalMatrix: np.array = np.zeros((len(self.List), len(self.List)))
-        for CausalEvent1 in self.List:
-            for CausalEvent2 in self.List:
+        self.CausalMatrix: np.array = np.zeros((len(self.ElementList), len(self.ElementList)))
+        for CausalEvent1 in self.ElementList:
+            for CausalEvent2 in self.ElementList:
                 self.CausalMatrix[CausalEvent1.label, CausalEvent2.label] = 1 if (spacetime_interval(CausalEvent1.coordinates, CausalEvent2.coordinates) < 0 and CausalEvent1.label < CausalEvent2.label) else 0
         
         self.LinkMatrix: np.array = self.find_linkmatrix()
         self.Interval: set(CausalEvent) = set() 
         
+        #TODO Add future and past of causal events
+        
     #VISUALIATION 
     
     def visualisation(self): 
-        if self.LinkMatrix == None: 
-            self.find_linkmatrix()
-        #Visualise(self.linkMatrix)
     
+        coordinates = np.array([x.coordinates for x in self.ElementList])
+        plt.scatter(coordinates[:, 1], coordinates[:, 0], s = 100)
+
+        for i in range(len(self.LinkMatrix)):
+            for j in range(len(self.LinkMatrix[i])):
+                if self.LinkMatrix[i][j] == 1:
+                    plt.plot([coordinates[i][1], coordinates[j][1]], [coordinates[i][0], coordinates[j][0]], color = 'green')
+        plt.xlabel('Space', fontsize = 40)
+        plt.ylabel('Time', fontsize = 40)
+        plt.xticks(fontsize = 30)
+        plt.yticks(fontsize = 30)
+        plt.show()
+            
     #CALCULATING LINK MATRIX
     
     def find_linkmatrix(self):
@@ -140,8 +156,12 @@ class CausalSet(object):
         print(f'Causal Event {l1} {state} Causal Event {l2}.')
         
 if __name__ == "__main__":
-    testcausalset = CausalSet() 
-    print(testcausalset.List)
-    print(testcausalset.CausalMatrix)
-    print(testcausalset.LinkMatrix)
+    tic = time.time()
+    c = CausalSet(number_of_points = 100) 
+    #print(c.ElementList)
+    print('Casual Matrix: \n', c.CausalMatrix)
+    print('Link Matrix: \n', c.LinkMatrix)
+    c.visualisation()
+    toc = time.time() 
+    print(f'Time elapsed is {toc - tic}')
         
