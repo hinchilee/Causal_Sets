1e3f3012d78288172a6fc37003002d05f1559805
hinchilee <66686518+hinchilee@users.noreply.github.com>
2022-11-28 01:04:04 +0000

    Add files via upload

---
+++ count_molecules_Dynamic.py
@@ -0,0 +1,48 @@
+
+from causalset import CausalSet
+import time
+import pandas as pd
+import multiprocessing as mp
+
+def count_chains(N):
+    return CausalSet(sprinkling_density=N, dimension=4, BHtype = 'Dynamic', T = 1).find_molecules()
+
+def main():
+    tic = time.time()
+
+    rho_array = [0.03, 0.1, 0.3, 1]
+    d = {}
+
+    for rho in rho_array:
+        # Number of realisations
+        n = 50
+
+        pool = mp.Pool(mp.cpu_count() - 8)
+        H_counts = pool.map(count_chains, [rho] * n)
+        pool.close()
+
+        max_H_i = max([len(H) for H in H_counts])
+        H_array = [0] * max_H_i
+        for i in range(max_H_i):
+            for H in H_counts:
+                if len(H) > i:
+                    H_array[i] += H[i] / n
+
+        d[rho] = H_array
+
+        print(rho)
+        toc = time.time()
+        print(f'Time elapsed is {toc - tic}')
+    
+    max_H_i = max([len(d[rho]) for rho in rho_array])
+    for rho in rho_array:
+        while len(d[rho]) < max_H_i:
+            d[rho].append(0)
+
+    df = pd.DataFrame(d)
+    df.index += 1
+    print(df)
+    df.to_csv('H_iDynamic.csv')
+
+if __name__ == "__main__":
+    main()
---
+++ count_molecules_Rindler.py
@@ -0,0 +1,47 @@
+from causalset import CausalSet
+import time
+import pandas as pd
+import multiprocessing as mp
+
+def count_chains(N):
+    return CausalSet(sprinkling_density=N, dimension=4, BHtype = 'Rindler').find_molecules()
+
+def main():
+    tic = time.time()
+
+    rho_array = [100, 300, 1000, 3000]
+    d = {}
+
+    for rho in rho_array:
+        # Number of realisations
+        n = 50
+
+        pool = mp.Pool(mp.cpu_count() - 8)
+        H_counts = pool.map(count_chains, [rho] * n)
+        pool.close()
+
+        max_H_i = max([len(H) for H in H_counts])
+        H_array = [0] * max_H_i
+        for i in range(max_H_i):
+            for H in H_counts:
+                if len(H) > i:
+                    H_array[i] += H[i] / n
+
+        d[rho] = H_array
+
+        print(rho)
+        toc = time.time()
+        print(f'Time elapsed is {toc - tic}')
+    
+    max_H_i = max([len(d[rho]) for rho in rho_array])
+    for rho in rho_array:
+        while len(d[rho]) < max_H_i:
+            d[rho].append(0)
+
+    df = pd.DataFrame(d)
+    df.index += 1
+    print(df)
+    df.to_csv('H_iRindler.csv')
+
+if __name__ == "__main__":
+    main()
