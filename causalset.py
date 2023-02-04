changed link counting to causal matrix (incorrect)

causalset.py
@@ -38,9 +38,13 @@ class CausalSet(object):
             # Normalised Sprinkling Volume to 1!!! Important to find out <N> then Poisson, not get N from Poisson then scale to area
 
             bounds = kwargs.get('bounds', np.array([[-0.5, 0.5] for i in range(kwargs.get('dimension', 2))]))
-            self.SpacetimeVolume = sum(bounds[:,1] - bounds[:,0])
+            #print('Boundaries', bounds)
+            self.SpacetimeVolume = np.prod(bounds[:,1] - bounds[:,0])
+            print('Spacetime volume', self.SpacetimeVolume)
             AveragePoints = self.SpacetimeVolume * (kwargs.get('sprinkling_density'))
-            sprinkledcoords = Sprinkling_Uniform(dimension=kwargs.get('dimension', 2), number_of_points=poisson.rvs(AveragePoints), bounds=bounds)
+            noPoints = poisson.rvs(AveragePoints)
+            print('Number of points:', noPoints)
+            sprinkledcoords = Sprinkling_Uniform(dimension=kwargs.get('dimension', 2), number_of_points=noPoints, bounds=bounds)
             self.wrapAroundLength = 1
 
         # FOR DYNAMIC BLACKHOLE HORIZON
@@ -242,14 +246,14 @@ class CausalSet(object):
     # CALCULATING MOLECULES
 
     def find_molecules(self):
-        if self.LinkMatrix is None:
-            self.find_linkmatrix()
+        # if self.LinkMatrix is None:
+        #     self.find_linkmatrix()
         maximals = []
         maximal_but_ones = []
 
         if self.BHtype == 'Rindler':
-            for i in range(len(self.LinkMatrix)):
-                links = sum(self.LinkMatrix[i])
+            for i in range(len(self.CausalMatrix)):
+                links = sum(self.CausalMatrix[i])
                 if links == 0:
                     maximals.append(i)
                 elif links == 1:
@@ -280,7 +284,7 @@ class CausalSet(object):
             for maximal in maximals:
                 if self.ElementList[maximal].coordinates[0] > self.ElementList[maximal].coordinates[1]:
                     count = 0
-                    for minimal_link in set(np.where(self.LinkMatrix[:, maximal] == 1)[0]).intersection(maximal_but_ones):
+                    for minimal_link in set(np.where(self.CausalMatrix[:, maximal] == 1)[0]).intersection(maximal_but_ones):
                         if self.ElementList[minimal_link].coordinates[0] < self.ElementList[minimal_link].coordinates[1]:
                             count += 1
                             min_time = min(
@@ -319,8 +323,8 @@ if __name__ == "__main__":
 
         tic = time.time()
 
-        c = CausalSet(sprinkling_density=2000,    # 0.1-1 for Dynamic Uniform, 1k - 10k for Dynamic Tube, 1k - 10k for Rindler, Empty
-                      dimension=4,
+        c = CausalSet(sprinkling_density=3000,    # 0.1-1 for Dynamic Uniform, 1k - 10k for Dynamic Tube, 1k - 10k for Rindler, Empty
+                      dimension=3,
                       BHtype='Rindler',           # 'Rindler', 'Dynamic', 'Empty'
                       sprinkling='Uniform',          # 'Uniform' or 'Tube' for 'Dynamic'BH
                       T=3)                        # T is only needed when BHtype = 'Dynamic'

count_molecules_Rindler.py
@@ -13,8 +13,9 @@ if len(sys.argv) > 1:
 
 def count_chains(N,mintime, d = 4):
     np.random.seed(int.from_bytes(os.urandom(4), byteorder='little'))
-    boundsArray = np.array([[-0.5, 0.5] for i in range(d)])
-    boundsArray[0][0] = boundsArray[0][1] + mintime #no normalisation
+    boundsArray = np.array([[-0.5, 1.0] for i in range(d)])
+    boundsArray[0][0] = 0.5 + mintime #no normalisation
+    boundsArray[0][1] = 0.5
     
     return CausalSet(sprinkling_density=N, dimension=d, BHtype='Rindler', bounds = boundsArray).find_molecules()
   
@@ -24,18 +25,19 @@ def main():
     with open(path + 'min_time.json') as f:
         min_times= json.load(f)
         
-    rho_array = [1000,3000]
-    d_array = [2,3,4]
+    rho_array = [30000]
+    d_array = [2]
     for rho in rho_array:
         for dimension in d_array:
         # Number of realisations
-            n = 20
+            n = 100
             try:
                 min_time = min(min_times[f"{str(rho)}_{dimension}d"])
                 if min_time == 10000: 
                     min_time = -1
             except: 
-                raise ValueError('No test run information!')
+                min_time = -0.2
+                #raise ValueError('No test run information!')
             
     
             for _i in range(n):

min_time.py
@@ -1,6 +1,7 @@
 from causalset import CausalSet
 import sys
 import json
+import numpy as np 
 
 path = ''
 if len(sys.argv) > 1:
@@ -10,15 +11,17 @@ def main():
     with open(path + 'min_time.json') as f:
         d = json.load(f)
 
-    for rho in [1000, 3000]:
-        for dimension in [2,3,4]:
+    for rho in [3000, 10000]:
+        for dimension in [2,3]:
             print('sprinkling density', rho)
             print('dimension:', dimension)
             if (f"{str(rho)}_{dimension}d") not in d.keys():
                 d[f"{str(rho)}_{dimension}d"] = []
         
             for i in range(5):
-                c = CausalSet(sprinkling_density=rho, dimension=dimension, BHtype = 'Rindler')
+                boundsArray = np.array([[-0.5, 1.5] for i in range(dimension)])
+                boundsArray[0][1] = 0.5
+                c = CausalSet(sprinkling_density=rho, dimension=dimension, BHtype = 'Rindler', bounds = boundsArray)
                 c.find_molecules()
                 d[f"{str(rho)}_{dimension}d"].append(c.min_time)
             