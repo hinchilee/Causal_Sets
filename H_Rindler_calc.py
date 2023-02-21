corrected molecule entropy calculation

H_Rindler_calc.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import itertools
-from causalsetfunctions import find_entropy
+from causalsetfunctions import find_entropy, linear
+import numpy as np
+import matplotlib.pyplot as plt 
+from scipy.optimize import curve_fit
 
 d_array = [2,3,4]
 for d in d_array: 
@@ -8,6 +11,8 @@ for d in d_array:
     #print(df)
     rho_array = df['rho'].unique()
     rho_array.sort()
+    y_entropyList = list() 
+    x = list()
     for rho in rho_array:
         
         print('\n sprinkling density',rho, f'in {d} dimensions')
@@ -15,6 +20,7 @@ for d in d_array:
         print(f'number of iterations: {iterationsNo}')
         totalHarray = [sum(x) for x in itertools.zip_longest(*[[int(x) if x != '' else 0 for x in a.replace('[', '').replace(']', '').split(', ')] for a in df[df['rho'] == rho]['H'].values], fillvalue=0)]
         print('total Harray \n', totalHarray)
+        print('p_i:', totalHarray/ np.sum(totalHarray))
         
         totalLinks = 0 
         for i, value in enumerate(totalHarray):
@@ -26,5 +32,21 @@ for d in d_array:
         
         print(f'Empirical a value {empiricalavalue}')
         
-        entropy = find_entropy(totalHarray)
+        entropy = find_entropy(totalHarray, iterationsNo)
         print(f'Entropy: {entropy}')
+        
+        y_entropyList.append(entropy)
+        x.append(1/rho**((2-d)/d))
+    
+    plt.scatter(x, y_entropyList)
+    plt.xlabel(r'$\frac{A}{\rho^{\frac{2-d}{d}}}$', fontsize = 20 )
+    plt.ylabel(r'$s_{Boltz}$', fontsize = 20 )
+    popt, pcov = curve_fit(linear, x, y_entropyList)
+    xLinspace = np.linspace(min(x), max(x), 100)
+    plt.plot(xLinspace, linear(xLinspace, *popt))
+    plt.title(f's_Boltzmann in Rindler in {d}d')
+    plt.show()
+    
+    print(f'\n \n \n a_Boltzmann value for {d}d is {popt[0]}')
+        
+        

causalsetfunctions.py
@@ -31,11 +31,11 @@ def spacetime_interval(stcoord1, stcoord2, BHtype , wrapAroundLength = 2):
     
     return spacetime_interval
 
-def find_entropy(H_array):
+def find_entropy(H_array, realisations):
     N = np.sum(H_array)
     p_array = H_array / N
     
-    return N * np.sum([p_i * np.log(1 / p_i) for p_i in p_array if p_i != 0])
+    return (N/realisations) * np.sum([p_i * np.log(1 / p_i) for p_i in p_array if p_i != 0])
 
 def inside_horizon(x):
     s = -(x[0] ** 2)
@@ -88,6 +88,9 @@ def n_sphere_surfacearea(n, r):
         
     return ans
     
+def linear(x, m, c): 
+    return m*x+c
+
 if __name__ == "__main__":
     # H_arr = np.array([2,1])
     # print(find_entropy(H_arr))