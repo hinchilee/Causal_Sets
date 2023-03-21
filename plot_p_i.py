3bd73e1bec8bd30b5861ceb824faf1fe7fc8c363
Hinchi Lee <66686518+hinchilee@users.noreply.github.com>
2023-03-21 23:52:43 +0000

    modified p_i error

--- plot_p_i.py
+++ plot_p_i.py
@@ -1,41 +1,63 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.stats import sem
+from scipy.stats import sem, tstd
 plt.rc('font', family='Arial', size=20)
 
-bar_index = np.arange(6)
+bar_index = np.arange(5)
 width = 0.35
-d_array = [2, 3, 4]
+d_array = [4]
 moleculetype = 'lambda'
 for BHtype in ['Rindler', 'Dynamic']:
     df = pd.concat([pd.read_csv(f'H_{BHtype}{d}d_{moleculetype}.csv', names=['rho', 'H'], header=None) for d in d_array])
     H_dict = {}
     N = df.shape[0]
-    for index, row in df.iterrows():
+    maxrho = max(df['rho'])
+    maxrhodf = df[df['rho'] == maxrho]
+    
+    # Initialise p_i dictionary
+    p_i_dict = dict()
+    for i in range(10): #sets i_max
+        p_i_dict[i+1] = list() 
+    
+    for index, row in maxrhodf.iterrows():
+    #for index, row in df.iterrows():
         H = [int(x) if x != '' else 0 for x in row['H'].replace('[', '').replace(']', '').split(', ')]
-        for i in range(len(H)):
-            if (i + 1) in H_dict:
-                H_dict[i + 1].append(H[i])
-            else:
-                H_dict[i + 1] = [H[i]]
-
+        # for i in range(len(H)):
+        #     if (i + 1) in H_dict:
+        #         H_dict[i + 1].append(H[i])
+        #     else:
+        #         H_dict[i + 1] = [H[i]]
+        
+        # Enter realisation p_i into dictionary
+        N = np.sum(H)
+        p_i = np.array(H)/ N
+        for i in range(10):
+            try:
+                p_i_dict[i+1].append(p_i[i])
+            except: 
+                p_i_dict[i+1].append(0)
+                
     mean_array = []
     std_array = []
-    for H_i in H_dict.values():
-        H_i = np.pad(H_i, (0, N - len(H_i)), 'constant')
-        mean_array.append(np.mean(H_i))
-        std_array.append(sem(H_i))
+    # for H_i in H_dict.values():
+    #     H_i = np.pad(H_i, (0, N - len(H_i)), 'constant')
+    #     mean_array.append(np.mean(H_i))
+    #     std_array.append(sem(H_i))
+    for p_i in p_i_dict.values():
+        p_i = np.pad(p_i, (0, len(p_i) - len(p_i)), 'constant')
+        mean_array.append(np.mean(p_i))
+        std_array.append(sem(p_i))
     std_array /= np.sum(mean_array)
     mean_array /= np.sum(mean_array)
     if BHtype == 'Rindler':
         padding = 0
     else:
         padding = width
-    plt.bar(bar_index + padding, mean_array[:6], width, yerr=std_array[:6], label=BHtype)
+    plt.bar(bar_index + padding, mean_array[:5], width, yerr=std_array[:5], label=BHtype, capsize = 4)
 plt.ylabel(r'$p_i$')
-plt.xlabel(r'$i$-molecule')
-plt.xticks(bar_index + width / 2, ['1', '2', '3', '4', '5', '6'])
+plt.xlabel(r'$i$-lambda')
+plt.xticks(bar_index + width / 2, ['1', '2', '3', '4', '5'])
 plt.legend()
 #plt.tight_layout()
 plt.savefig(f'Plots/p_i_against_i.png', dpi = 300, transparent = True, bbox_inches='tight')
