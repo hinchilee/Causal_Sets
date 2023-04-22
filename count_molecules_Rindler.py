45f9f1b1c105675a27216f94c9111d60608ea08e
Hinchi Lee <66686518+hinchilee@users.noreply.github.com>
2023-04-22 18:18:17 +0100

    Add files via upload

--- count_molecules_Rindler.py
+++ count_molecules_Rindler.py
@@ -37,53 +37,63 @@ def count_chains(N, mintime, mindistance, maxdistance, moleculetype, boundsArray
 def main():
     tic = time.time()
     #d_array = [2]
-    d_array = [4]
-    moleculeType = 'v'
+    d_array = [3,4]
+    moleculeType = 'lambda'
     #N_max = 1000
-    N_max = 7000
+    N_max = 1000
+    b = 4
+    rho2 = 10
     #for N_max in [19000]:
     for dimension in d_array:
-        if dimension == 4: 
-            rho_array = [1000, 30000]
-        for rho in rho_array:
-        # Number of realisations
-            n = 100
-            if rho == 30000: 
-                b = 2.8
-                N_max = 10000
-            elif rho == 1000:
-                b = 3
-                N_max = 8000
-            #rho = N_max 
-            #if rho == 1000: 
-             #   rho -= 100
-            # try:
-            #     df = pd.read_csv(path + f'TestRun/test_run_Rindler_rho{rho}_{dimension}d.csv', names=['type', 'value'], header=None)
-            #     with open(path + 'min_time.json') as f:
-            #         dfTime = json.load(f)
-            #     min_time = max(dfTime[f"{rho}_{dimension}d"], -1)
-            #     min_distance = max(df[df['type'] == 'min_distance']['value'].min()*0.95, 0)
-            #     max_distance = min(df[df['type'] == 'max_distance']['value'].max()*1.05, 2)
 
-            # except:
-                
-            #     raise ValueError ('No test run information!')
-            
-            boundsArray, adjusted_rho, l, adjusted_l = compute_spacetimecuts_uniform_Rindler(d = dimension, rho = rho, N_max = N_max, b= b)
+        if dimension == 3:
+            N_max_array = [8000, 10000] 
+            n = 100
+            c = 4
         
-            for _i in range(n):
-                print(f'\n realisation:{_i+1}, rho:{adjusted_rho}, dimension:{dimension}')
-                print('BoundsArray:\n', boundsArray)
-                print('N_max:', N_max)
-                print(f'l: {l}, adjustedl: {adjusted_l}')
-                if moleculeType == 'lambda' or moleculeType == 'v':
-                    H , b, V, Subgraphs, Connected, b2 = count_chains(adjusted_rho, 0, 0, 0, moleculeType, boundsArray, dimension)
-                with open(path + f'H_Rindler{dimension}d_lambda.csv', 'a') as f:
-                    writer = csv.writer(f, lineterminator='\n')
-                    writer.writerow([adjusted_rho, H, b])
-                with open(path + f'H_Rindler{dimension}d_v.csv', 'a') as f:
-                    writer = csv.writer(f, lineterminator='\n')
-                    writer.writerow([adjusted_rho, V, Subgraphs, Connected, b2])
+            # d= 3( to be deleted after)
+            for N_max in N_max_array:
+            # Number of realisations
+                boundsArray, adjusted_rho, l= compute_spacetimecuts_uniform_Rindler(d = dimension, rho2 = rho2, N_max = N_max, b= c)
+            
+                for _i in range(n):
+                    print(f'\n realisation:{_i+1}, rho:{adjusted_rho}, dimension:{dimension}')
+                    print('BoundsArray:\n', boundsArray)
+                    print('N_max:', N_max)
+                    print(f'l: {l}')
+                    if moleculeType == 'lambda' or moleculeType == 'v':
+                        H , b, V, Subgraphs, Connected, b2 = count_chains(adjusted_rho, 0, 0, 0, moleculeType, boundsArray, dimension)
+                    with open(path + f'H_Rindler{dimension}d_lambda.csv', 'a') as f:
+                        writer = csv.writer(f, lineterminator='\n')
+                        writer.writerow([adjusted_rho, H, b])
+                    with open(path + f'H_Rindler{dimension}d_v.csv', 'a') as f:
+                        writer = csv.writer(f, lineterminator='\n')
+                        writer.writerow([adjusted_rho, V, Subgraphs, Connected, b2])
+          
+                    
+        elif dimension == 4: 
+            b_array = [3.5, 3.1, 2.7, 2.4]
+            n = 100
+            N_max = 10000
+            
+            # d= 4( to be deleted after)
+            for c in b_array:
+            # Number of realisations
+                boundsArray, adjusted_rho, l= compute_spacetimecuts_uniform_Rindler(d = dimension, rho2 = rho2, N_max = N_max, b= c)
+            
+                for _i in range(n):
+                    print(f'\n realisation:{_i+1}, rho:{adjusted_rho}, dimension:{dimension}')
+                    print('BoundsArray:\n', boundsArray)
+                    print('N_max:', N_max)
+                    print(f'l: {l}')
+                    if moleculeType == 'lambda' or moleculeType == 'v':
+                        H , b, V, Subgraphs, Connected, b2 = count_chains(adjusted_rho, 0, 0, 0, moleculeType, boundsArray, dimension)
+                    with open(path + f'H_Rindler{dimension}d_lambda.csv', 'a') as f:
+                        writer = csv.writer(f, lineterminator='\n')
+                        writer.writerow([adjusted_rho, H, b])
+                    with open(path + f'H_Rindler{dimension}d_v.csv', 'a') as f:
+                        writer = csv.writer(f, lineterminator='\n')
+                        writer.writerow([adjusted_rho, V, Subgraphs, Connected, b2])
                 
     toc = time.time()
     print(f'Time taken is {toc - tic}')
